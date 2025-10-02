b34e3353792ef5ec693f011609e28365aaa85f8b
devenock <enockomondi305@gmail.com>
2025-10-02 08:25:06 +0300

    solving two sum II leetcode challenge using two pointers pattern

--- problemSolvingPatterns/multiple_pointers/multiplePointers.js
+++ problemSolvingPatterns/multiple_pointers/multiplePointers.js
@@ -9,40 +9,43 @@
 
 // SET OF PROBLEMS ON LEETCODE
 // 1. Two Sum II(Easy)
-// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-// You may assume that each input would have exactly one solution, and you may not use the same element twice.
-// You can return the answer in any order.
+// Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order,
+// find two numbers such that they add up to a specific target number. Let these two numbers be
+// numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+// Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+// The tests are generated such that there is exactly one solution. You may not use the same element twice.
+// Your solution must use only constant extra space.
+
 // Example 1:
-Input: ((nums = [2, 7, 11, 15]), (target = 9));
-Output: [0, 1];
-// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-// Example 2:
-Input: ((nums = [3, 2, 4]), (target = 6));
+Input: ((numbers = [2, 7, 11, 15]), (target = 9));
 Output: [1, 2];
+// Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+
+// Example 2:
+Input: ((numbers = [2, 3, 4]), (target = 6));
+Output: [1, 3];
+// Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
+
 // Example 3:
-Input: ((nums = [3, 3]), (target = 6));
-Output: [0, 1];
+Input: ((numbers = [-1, 0]), (target = -1));
+Output: [1, 2];
+// Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
 
 // SOLUTION
-function twoSum(nums, target) {
-  // brute force(nested for loop => 0(n^2))
-  // if(nums.length === 0){
-  //   return 0;
-  // }
-  // for (let i = 0; i < nums.length; i++){
-  //   for (let j = i + 1; j < nums.length; j++){
-  //     if(nums[i] + nums[j] === target){
-  //       return [i, j]
+function twoSumII(nums, target) {
+  // brute force => 0(n^2)
+  // for (let i = 0; i < nums.length; i++) {
+  //   for (let j = i + 1; j < nums.length; j++) {
+  //     if (nums[i] + nums[j] === target) {
+  //       return [i, j];
   //     }
   //   }
   // }
-
-  //optimized solution(two pointers => 0(n))
-  // you can decide to sort the arry first if it is not sorted
-  nums.sort((a, b) => a - b);
+  //
+  // optimized solution => 0(n)
   let firstPointer = 0;
   let lastPointer = nums.length - 1;
-  // use while loop to acertain some condition
+
   while (firstPointer < lastPointer) {
     let sum = nums[firstPointer] + nums[lastPointer];
     if (sum === target) {

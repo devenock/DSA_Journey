1f80f4784c6bcdd08596d281382143fef90f6afd
devenock <enockomondi305@gmail.com>
2026-07-28 07:45:46 +0300

    adding 3sum solution

--- problemSolvingPatterns/multiple_pointers/multiplePointers.js
+++ problemSolvingPatterns/multiple_pointers/multiplePointers.js
@@ -95,6 +95,57 @@ function isPalindrome(s) {
   return true;
 }
 // 3. 3Sum(Medium)
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] where nums[i] + nums[j] + nums[k] == 0, and the indices i, j and k are all distinct.
+// The output should not contain any duplicate triplets. You may return the output and the triplets in any order.
+// Example 1:
+// Input: nums = [-1,0,1,2,-1,-4]
+// Output: [[-1,-1,2],[-1,0,1]]
+// Explanation:
+// nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+// nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+// nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+// The distinct triplets are [-1,0,1] and [-1,-1,2].
+// Example 2:
+// Input: nums = [0,1,1]i
+// Output: []
+// Explanation: The only possible triplet does not sum up to 0.
+// Example 3:
+// Input: nums = [0,0,0]
+// Output: [[0,0,0]]
+// Explanation: The only possible triplet sums up to 0.
+// SOLUTION
+function threeSum(nums) {
+  // sort the array first
+  nums.sort((a, b) => a - b)
+  let result = []
+  // loop through the array first
+  for (let i = 0; i < nums.length; i++){
+    // check for a posibility
+    if (nums[i] > 0) break;
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    // define the pointers
+    let left = i + 1
+    let right = nums.length - 1
+    // set the loop condition
+    while (left < right) {
+      let sum = nums[i] + nums[left] + nums[right]
+      if (sum > 0) {
+        right--
+      } else if (sum < 0) {
+        left++
+      } else {
+        result.push([nums[i], nums[left], nums[right]])
+        left++
+        right--
+        while (left < right && nums[left] === nums[left - right]) {
+          left++
+        }
+      }
+    }
+  }
+
+  return result;
+}
 
 // 4. Container With Most Water(Medium)
 // You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
@@ -118,10 +169,10 @@ function containerWithMostWater(height) {
   // define the pointers
   let left = 0
   let right = height.length - 1
-  let height = Math.min(height[left], height[right])
   let result = 0
   while (left < right) {
     let width = right - left
+     let height = Math.min(height[left], height[right])
     let area = width * height;
     result = Math.max(result, area)
     if (height[left] <= height[right]) {
